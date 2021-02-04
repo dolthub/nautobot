918851071856306838a7eda401b492af47de795f
Andy Arthur <andy@dolthub.com>
2021-02-04 14:08:16 -0800

    cleanup

--- nautobot/ipam/models.py
+++ nautobot/ipam/models.py
@@ -2,7 +2,6 @@ import netaddr
 from django.conf import settings
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.postgres.fields import ArrayField
 from django.core.exceptions import ValidationError
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
